64a5b4c2c782d88bb530bea7fe8019a6d5a2f527
ElDiegu <eldieguuu@gmail.com>
2024-06-20 20:02:37 +0200

    Input Manger v1.0
    Basic Script to manage player input using the new Unity Input System

---
+++ MushyAndCoffe/Assets/_Project/Gameplay Scripts/Managers/InputManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+
+namespace MushyAndCoffe.Managers
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] private PlayerInput playerInput;
+        public InputStruct InputStruct { get; private set; }
+        
+        private void Awake()
+        {
+            if (!playerInput) playerInput = GetComponent<PlayerInput>();
+            InputStruct = new InputStruct(playerInput);
+        }
+        
+        private void Update()
+        {
+            GatherInput();
+            DebugManager.StaticDebug(MessageTypes.Input, InputStruct.ToString());
+        }
+        
+        private void GatherInput() 
+        {
+            InputStruct = new InputStruct(playerInput);
+        }
+    }
+    
+    public struct InputStruct
+    {
+        public PlayerInput PlayerInput { get; set; }
+        public Vector2 Movement { get; set; }
+        public bool Interact { get; set; }
+        public bool Dash { get; set; }
+        
+        public InputStruct(PlayerInput playerInput) 
+        {
+            PlayerInput = playerInput;
+            Movement = PlayerInput.actions["Movement"].ReadValue<Vector2>();
+            Interact = PlayerInput.actions["Interaction"].triggered;
+            Dash = PlayerInput.actions["Dash"].triggered;
+        }
+        
+        public override string ToString() 
+        {
+            return $"Movement {Movement} | Interaction {Interact} | Dash {Dash}";
+        }
+    }
+}
