7df0f6821b6351ecebd397ed4039d1b438e9f3c8
ElDiegu <eldieguuu@gmail.com>
2024-09-17 10:20:53 +0200

    PlayerParameters Interaction Parameters

--- MushyAndCoffe/Assets/_Project/Gameplay Scripts/Player Scripts/PlayerParameters.cs
+++ MushyAndCoffe/Assets/_Project/Gameplay Scripts/Player Scripts/PlayerParameters.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 namespace MushyAndCoffe.Player
 {
-	public class PlayerParameters : PersistentSingleton<PlayerParameters>
-	{
-		[Header("Movement Parameters")]
-		public float acceleration = 1f;
-		public float deceleration = 2f;
-		public float maxSpeed = 5f;
-		public float dashStrength = 5f;
-		public float turnSpeed = 360f;
+    public class PlayerParameters : PersistentSingleton<PlayerParameters>
+    {
+        [Header("Movement Parameters")]
+        public float acceleration = 1f;
+        public float deceleration = 2f;
+        public float maxSpeed = 5f;
+        public float dashStrength = 5f;
+        public float turnSpeed = 360f;
+  
+        [Header("Interaction Parameters")]      
+        public float interactionDistances = 1f;
+        public float interactionRadius = 1f;
+        public LayerMask interactionLayerMask;
 
-	}
+    }
 }
