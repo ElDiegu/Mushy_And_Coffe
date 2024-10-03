ec5abd8f52f9341ec518164c31e8ac2fcaa41d38
ElDiegu <eldieguuu@gmail.com>
2024-10-03 12:03:54 +0200

    Removed AddressableAsstes Package as it caused errors while managing ScriptableObjects

--- MushyAndCoffe/Assets/_Project/Databases/FurnitureDatabase.cs
+++ MushyAndCoffe/Assets/_Project/Databases/FurnitureDatabase.cs
@@ -2,23 +2,21 @@ using System.Collections.Generic;
 using System.Linq;
 using MushyAndCoffe.ScriptableObjects;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace MushyAndCoffe.Databases
 {
-    public static class FurnitureDatabase
-    {
-        public static List<FurnitureSO> Furnitures { get; private set; } = Resources.LoadAll<FurnitureSO>("Scriptable Objects/Furniture").ToList();
-        
-        public static FurnitureSO FindByID(int ID) 
-        {
-            return Furnitures.FirstOrDefault(furniture => furniture.ID == ID);
-        } 
-        
-        public static FurnitureSO FindByName(string name) 
-        {
-            return Furnitures.FirstOrDefault(furniture => furniture.Name == name); 
-        }
-    }
+	public static class FurnitureDatabase
+	{
+		public static List<FurnitureSO> Furnitures { get; private set; } = Resources.LoadAll<FurnitureSO>("Scriptable Objects/Furniture").ToList();
+		
+		public static FurnitureSO FindByID(int ID) 
+		{
+			return Furnitures.FirstOrDefault(furniture => furniture.ID == ID);
+		} 
+		
+		public static FurnitureSO FindByName(string name) 
+		{
+			return Furnitures.FirstOrDefault(furniture => furniture.Name == name); 
+		}
+	}
 }
